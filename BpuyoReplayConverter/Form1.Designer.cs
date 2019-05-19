d093968efd45bc5fb1ba8a040bfdf6c50e9ff591
nsendoda <n.sendoda@gmail.com>
2019-05-19 21:36:06 +0900

    v1.0.0

--- BpuyoReplayConverter/Form1.Designer.cs
+++ BpuyoReplayConverter/Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(514, 30);
             this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "ここに出力経過が表示されます";
+            this.richTextBox1.Text = "ここに出力結果が表示されます";
             // 
             // InputPlayerNameText
             // 
@@ -62,7 +62,7 @@
             this.InputPlayerNameText.Name = "InputPlayerNameText";
             this.InputPlayerNameText.Size = new System.Drawing.Size(514, 28);
             this.InputPlayerNameText.TabIndex = 2;
-            this.InputPlayerNameText.Text = "UENNI";
+            this.InputPlayerNameText.Text = "";
             // 
             // FolderPathText
             // 
@@ -71,7 +71,7 @@
             this.FolderPathText.Name = "FolderPathText";
             this.FolderPathText.Size = new System.Drawing.Size(514, 32);
             this.FolderPathText.TabIndex = 3;
-            this.FolderPathText.Text = "G:\\GoogleDrive\\game\\puzzle\\Bpuyo\\rec\\UENNI";
+            this.FolderPathText.Text = "";
             this.FolderPathText.TextChanged += new System.EventHandler(this.FolderPathText_TextChanged);
             // 
             // FolderSetButton
@@ -96,11 +96,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label3.Location = new System.Drawing.Point(104, 24);
+            this.label3.Location = new System.Drawing.Point(75, 27);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(317, 16);
+            this.label3.Size = new System.Drawing.Size(368, 16);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Bぷよのリプレイファイル(prd)をぷよ譜へ変換機";
+            this.label3.Text = "Bぷよのリプレイファイル(prd)をぷよ譜（toml）へ変換機";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // checkBox1
@@ -114,7 +114,7 @@
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // Form1
+            // ApplicationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -127,7 +127,7 @@
             this.Controls.Add(this.InputPlayerNameText);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "ApplicationForm";
             this.Text = "BpuyoReplayConverter";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
